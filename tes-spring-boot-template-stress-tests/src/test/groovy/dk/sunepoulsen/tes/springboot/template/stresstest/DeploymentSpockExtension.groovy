ba1b9938cfba00cb1debbc4eaa038f6ea59c1cc7
sunepoulsen <sunepoulsen@gmail.com>
2021-12-12 02:38:32 +0100

    Add measurement of POST /templates to the Stress Test

--- tes-spring-boot-template-stress-tests/src/test/groovy/dk/sunepoulsen/tes/springboot/template/stresstest/DeploymentSpockExtension.groovy
+++ tes-spring-boot-template-stress-tests/src/test/groovy/dk/sunepoulsen/tes/springboot/template/stresstest/DeploymentSpockExtension.groovy
@@ -1,13 +1,18 @@
 package dk.sunepoulsen.tes.springboot.template.stresstest
 
+import groovy.util.logging.Slf4j
 import org.spockframework.runtime.extension.IGlobalExtension
 import org.spockframework.runtime.model.SpecInfo
+import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
 
+@Slf4j
 class DeploymentSpockExtension implements IGlobalExtension {
     private static GenericContainer templateBackendContainer = null
+    private static GenericContainer postgresqlContainer = null
 
     static GenericContainer templateBackendContainer() {
         return templateBackendContainer
@@ -15,11 +20,27 @@ class DeploymentSpockExtension implements IGlobalExtension {
 
     @Override
     void start() {
-        DockerImageName imageName = DockerImageName.parse('tes-spring-boot-template-backend-service:1.0.0-SNAPSHOT')
+        DockerImageName imageName
+
+        Network network = Network.newNetwork()
+
+        imageName = DockerImageName.parse('postgres:latest')
+        postgresqlContainer = new GenericContainer<>(imageName)
+            .withEnv('POSTGRES_DB', 'stress-test')
+            .withEnv('POSTGRES_USER', 'template')
+            .withEnv('POSTGRES_PASSWORD', 'jukilo90')
+            .withExposedPorts(5432)
+            .withNetwork(network)
+            .withNetworkAliases('postgres')
+        postgresqlContainer.start()
+
+        imageName = DockerImageName.parse('tes-spring-boot-template-backend-service:1.0.0-SNAPSHOT')
         templateBackendContainer = new GenericContainer<>(imageName)
-            .withEnv('SPRING_PROFILES_ACTIVE', 'ct')
+            .withEnv('SPRING_PROFILES_ACTIVE', 'stress-test')
             .withEnv('JAVA_OPTS', '-agentlib:jdwp=transport=dt_socket,address=8000,suspend=n,server=y')
+            .withClasspathResourceMapping('/config/application-stress-test.yml', '/app/resources/application-stress-test.yml', BindMode.READ_ONLY)
             .withExposedPorts(8000, 8080)
+            .withNetwork(network)
             .waitingFor(
                 Wait.forHttp('/actuator/health')
                     .forStatusCode(200)
@@ -35,5 +56,7 @@ class DeploymentSpockExtension implements IGlobalExtension {
     void stop() {
         templateBackendContainer.copyFileFromContainer('/app/logs/service.log', 'build/logs/tes-spring-boot-template-backend-service.log')
         templateBackendContainer.stop()
+
+        postgresqlContainer.stop()
     }
 }
