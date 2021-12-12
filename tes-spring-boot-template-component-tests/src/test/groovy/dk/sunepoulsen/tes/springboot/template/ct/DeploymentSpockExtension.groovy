5cbf944e036c3f8ee755d87e600e4917886cbac6
sunepoulsen <sunepoulsen@gmail.com>
2021-12-12 02:38:16 +0100

    Add tests of POST /templates to the Component Test

--- tes-spring-boot-template-component-tests/src/test/groovy/dk/sunepoulsen/tes/springboot/template/ct/DeploymentSpockExtension.groovy
+++ tes-spring-boot-template-component-tests/src/test/groovy/dk/sunepoulsen/tes/springboot/template/ct/DeploymentSpockExtension.groovy
@@ -2,33 +2,59 @@ package dk.sunepoulsen.tes.springboot.template.ct
 
 import dk.sunepoulsen.tes.springboot.client.core.rs.integrations.TechEasySolutionsBackendIntegrator
 import dk.sunepoulsen.tes.springboot.client.core.rs.integrations.TechEasySolutionsClient
+import dk.sunepoulsen.tes.springboot.template.client.rs.TemplateIntegrator
+import groovy.util.logging.Slf4j
 import org.spockframework.runtime.extension.IGlobalExtension
 import org.spockframework.runtime.model.SpecInfo
+import org.testcontainers.containers.BindMode
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.Network
 import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
 
+@Slf4j
 class DeploymentSpockExtension implements IGlobalExtension {
     private static GenericContainer templateBackendContainer = null
+    private static GenericContainer postgresqlContainer = null
+
+    static GenericContainer postgresqlContainer() {
+        return postgresqlContainer
+    }
 
     static GenericContainer templateBackendContainer() {
         return templateBackendContainer
     }
 
-    static TechEasySolutionsBackendIntegrator templateBackendIntegrator() {
+    static TemplateIntegrator templateBackendIntegrator() {
         String baseUrl = "http://${templateBackendContainer().host}:${templateBackendContainer().getMappedPort(8080)}"
         TechEasySolutionsClient client = new TechEasySolutionsClient(new URI(baseUrl))
 
-        return new TechEasySolutionsBackendIntegrator(client)
+        return new TemplateIntegrator(client)
     }
 
     @Override
     void start() {
-        DockerImageName imageName = DockerImageName.parse('tes-spring-boot-template-backend-service:1.0.0-SNAPSHOT')
+        DockerImageName imageName
+
+        Network network = Network.newNetwork()
+
+        imageName = DockerImageName.parse('postgres:latest')
+        postgresqlContainer = new GenericContainer<>(imageName)
+            .withEnv('POSTGRES_DB', 'ct')
+            .withEnv('POSTGRES_USER', 'template')
+            .withEnv('POSTGRES_PASSWORD', 'jukilo90')
+            .withExposedPorts(5432)
+            .withNetwork(network)
+            .withNetworkAliases('postgres')
+        postgresqlContainer.start()
+
+        imageName = DockerImageName.parse('tes-spring-boot-template-backend-service:1.0.0-SNAPSHOT')
         templateBackendContainer = new GenericContainer<>(imageName)
             .withEnv('SPRING_PROFILES_ACTIVE', 'ct')
             .withEnv('JAVA_OPTS', '-agentlib:jdwp=transport=dt_socket,address=8000,suspend=n,server=y')
+            .withClasspathResourceMapping('application-ct.yml', '/app/resources/application-ct.yml', BindMode.READ_ONLY)
             .withExposedPorts(8000, 8080)
+            .withNetwork(network)
             .waitingFor(
                 Wait.forHttp('/actuator/health')
                     .forStatusCode(200)
@@ -44,5 +70,7 @@ class DeploymentSpockExtension implements IGlobalExtension {
     void stop() {
         templateBackendContainer.copyFileFromContainer('/app/logs/service.log', 'build/logs/tes-spring-boot-template-backend-service.log')
         templateBackendContainer.stop()
+
+        postgresqlContainer.stop()
     }
 }
---
+++ tes-spring-boot-template-component-tests/src/test/groovy/dk/sunepoulsen/tes/springboot/template/ct/TemplatesSpec.groovy
@@ -0,0 +1,49 @@
+package dk.sunepoulsen.tes.springboot.template.ct
+
+import dk.sunepoulsen.tes.springboot.client.core.rs.exceptions.ClientBadRequestException
+import dk.sunepoulsen.tes.springboot.client.core.rs.model.ServiceError
+import dk.sunepoulsen.tes.springboot.template.client.rs.model.TemplateModel
+import spock.lang.Specification
+
+class TemplatesSpec extends Specification {
+
+    void "POST /templates returns OK"() {
+        given: 'Template service is available'
+            DeploymentSpockExtension.templateBackendContainer().isHostAccessible()
+
+            TemplateModel model = new TemplateModel(
+                name: 'name',
+                description: 'description'
+            )
+
+        when: 'Call POST /templates'
+            TemplateModel result = DeploymentSpockExtension.templateBackendIntegrator().create(model).blockingGet()
+
+        then: 'Verify health body'
+            result.id > 0L
+            result == new TemplateModel(
+                id: result.id,
+                name: 'name',
+                description: 'description'
+            )
+    }
+
+    void "POST /templates returns BAD_REQUEST"() {
+        given: 'Template service is available'
+            DeploymentSpockExtension.templateBackendContainer().isHostAccessible()
+
+        when: 'Call POST /templates'
+            DeploymentSpockExtension.templateBackendIntegrator().create(
+                new TemplateModel(
+                    description: 'description'
+                )
+            ).blockingGet()
+
+        then: 'Verify exception for bad_request'
+            ClientBadRequestException exception = thrown(ClientBadRequestException)
+            exception.serviceError == new ServiceError(
+                param: 'name',
+                message: 'must not be null'
+            )
+    }
+}
