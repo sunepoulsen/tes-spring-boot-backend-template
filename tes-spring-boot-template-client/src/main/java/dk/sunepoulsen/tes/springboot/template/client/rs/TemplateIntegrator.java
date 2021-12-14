7d42e1f7487e9aec6f8210b9b1c4176405f06fc8
sunepoulsen <sunepoulsen@gmail.com>
2021-12-14 20:56:37 +0100

    Extend client with the endpoint GET /templates

--- tes-spring-boot-template-client/src/main/java/dk/sunepoulsen/tes/springboot/template/client/rs/TemplateIntegrator.java
+++ tes-spring-boot-template-client/src/main/java/dk/sunepoulsen/tes/springboot/template/client/rs/TemplateIntegrator.java
@@ -2,8 +2,13 @@ package dk.sunepoulsen.tes.springboot.template.client.rs;
 
 import dk.sunepoulsen.tes.springboot.client.core.rs.integrations.TechEasySolutionsBackendIntegrator;
 import dk.sunepoulsen.tes.springboot.client.core.rs.integrations.TechEasySolutionsClient;
+import dk.sunepoulsen.tes.springboot.client.core.rs.model.PaginationResult;
 import dk.sunepoulsen.tes.springboot.template.client.rs.model.TemplateModel;
 import io.reactivex.Single;
+import org.springframework.data.domain.Pageable;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class TemplateIntegrator extends TechEasySolutionsBackendIntegrator {
     public TemplateIntegrator(TechEasySolutionsClient httpClient) {
@@ -15,4 +20,30 @@ public class TemplateIntegrator extends TechEasySolutionsBackendIntegrator {
                 .onErrorResumeNext(this::mapClientExceptions);
     }
 
+    public Single<PaginationResult> findAll() {
+        return findAll(null);
+    }
+
+    public Single<PaginationResult> findAll(Pageable pageable) {
+        StringBuffer url = new StringBuffer();
+        url.append("/templates");
+
+        if (pageable != null && pageable.isPaged()) {
+            url.append("?page=");
+            url.append(pageable.getPageNumber());
+            url.append("&size=");
+            url.append(pageable.getPageSize());
+
+            pageable.getSort().forEach(order -> {
+                url.append("&sort=");
+                url.append(URLEncoder.encode(order.getProperty(), StandardCharsets.UTF_8));
+                if (order.isDescending()) {
+                    url.append(",desc");
+                }
+            });
+        }
+
+        return Single.fromFuture(httpClient.get(url.toString(), PaginationResult.class))
+            .onErrorResumeNext(this::mapClientExceptions);
+    }
 }
